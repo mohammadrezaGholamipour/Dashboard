01ecded1a2c7ac3e09f7247ddf7c6a5a8634501d
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2023-06-01 16:53:27 +0330

    darkAndLightMood

--- src/components/header/tools.vue
+++ src/components/header/tools.vue
@@ -1,11 +1,12 @@
 <script setup>
 import BreadCrumb from './bread-crumb.vue'
 import ScreenSize from './screen-size.vue'
-
+import Theme from './theme.vue';
 </script>
 <template>
   <div class="tools">
     <BreadCrumb />
+    <Theme />
     <ScreenSize />
   </div>
 </template>
